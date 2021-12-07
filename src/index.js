5823411c93d0c50e73aec29ecd9e3aa1e3055fcb
RamiroIsaJ <ramfer.isaj@gmail.com>
2021-12-07 18:33:45 -0300

    agregado rutas y middlewares

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 // creo una instancia de express
 const app = express();
@@ -7,6 +9,22 @@ app.set('port', process.env.PORT || 4000);
 // escuchar el ṕuerto
 app.listen(app.get('port'), ()=>{
   console.log('Estoy en el puerto ' + app.get('port'));
-})
+});
+
+// middlewares o configuraciones extras
+app.use(morgan('dev')); // da información extra a la terminal
+app.use(cors()); // se encarga de recibir peticiones externas
 
 
+// permite recibir objetos en formato JSON
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// agregar rutas
+app.get('/', (req, res)=>{
+  res.send('hola desde el servidor');
+});
+
+app.delete('/borrar', (req, res)=>{
+  res.send('aquí se borrará datos');
+});
